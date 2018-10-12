bffcbac75d49d226f4143e2a3b07ff1541f6a03d
NINPXO <36712075+NINPXO@users.noreply.github.com>
2018-10-13 02:14:30 +0530

    Add files via upload

---
+++ huff.h
@@ -0,0 +1,134 @@
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class node
+{
+    unsigned char x;
+    unsigned int frequency;
+    unsigned char min;
+    node * left;
+    node * right;
+public:
+    node()
+    {
+
+    }
+
+    node(const node &n)
+    {
+        x = n.x;
+        frequency = n.frequency;
+        left = n.left;
+        right = n.right;
+    }
+
+    node(unsigned char d, unsigned int f): x(d), frequency(f), min(d)
+    {
+
+    }
+
+    node(node *, node *);
+
+    void cb(string *, string &);
+
+    bool operator> (const node &);
+};
+
+class heap
+{
+    node **minheap;
+    int n;
+public:
+    heap()
+    {
+        n = 0;
+        minheap = new node*[257];
+    }
+    void push(node *);
+    int size()
+    {
+        return n;
+    }
+    void pop();
+    node * top()
+    {
+        return minheap[1];
+    }
+};
+
+void node:: cb(string * codebook, string &code)
+{
+    if(!left && !right)
+    {
+        codebook[x] = code;
+        return;
+    }
+    if(left)
+    {
+        code += '0';
+        left->cb(codebook, code);
+        code.erase(code.end()-1);
+    }
+    if(right)
+    {
+        code += '1';
+        right->cb(codebook, code);
+        code.erase(code.end()-1);
+    }
+}
+
+node:: node(node * rc, node * lc)
+{
+    frequency = rc->frequency + lc->frequency;
+    right = rc;
+    left = lc;
+    min = (rc->min < lc->min) ? rc->min : lc->min;
+}
+
+void heap:: push(node *newnode)
+{
+    int temp = ++n;
+    while (temp != 1 && *minheap[temp / 2] > *newnode)
+    {
+        minheap[temp] = minheap[temp / 2];
+        temp = temp / 2;
+    }
+    minheap[temp] = newnode;
+}
+
+void heap:: pop()
+{
+    node *p = minheap[n];
+    minheap [n--] = minheap[1];
+    int temp = 1;
+    int child = 2;
+
+    while (child <= n)
+    {
+        if (child < n && *minheap[child] > *minheap[child + 1])
+            child++;
+
+        if (*minheap[child] > *p)
+            break;
+
+        minheap[temp] = minheap[child];
+        temp = child;
+        child *= 2;
+    }
+
+    minheap[temp] = p;
+}
+
+bool node::operator> (const node &rhs)
+{
+    if(frequency > rhs.frequency)
+        return true;
+    if(frequency < rhs.frequency)
+        return false;
+    if(frequency == rhs.frequency)
+        if(min > rhs.min)
+            return true;
+    return false;
+}
---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include "huff.h"
+#include <iostream>
+
+void compress();
+void encode();
+void decompress();
+node * constructHeap();
+unsigned int frequency[256] = {0};
+string codebook[256];
+
+int main(int argc, char* argv[])
+{
+    if(argc == 4)
+    {
+        decompress();
+    }
+    else
+        compress();
+    return 0;
+}
+
+void compress()
+{
+    unsigned char x;
+    cin >> noskipws;
+    while(cin >> x)
+        frequency[x]++;
+
+
+    node * root = constructheap();
+    string code;
+    root->cb(codebook,code);
+
+    encode();
+}
+
+void encode()
+{
+    unsigned int i;
+    for(i = 0; i < 256; i++)
+    {
+        cout<<(char) (0x000000ff & frequency[i]);
+        cout<<(char) ((0x0000ff00 & frequency[i]) >> 8);
+        cout<<(char) ((0x00ff0000 & frequency[i]) >> 16);
+        cout<<(char) ((0xff000000 & frequency[i]) >> 24);
+    }
+
+    unsigned char x;
+    char y = 0;
+    int z = 0;
+
+    cin.clear();
+    cin.seekg(0);
+    cin >> noskipws;
+    while(cin >> x){
+        for(i = 0; i < codebook[x].size(); i++, z++){
+            if(z == 8){
+                cout<< y;
+                y = 0;
+                z = 0;
+            }
+            if(codebook[x][i] == '1')
+                y = y | (0x01 << z);
+        }
+    }
+    if(z)
+        cout << y;
+}
+
+void decompress()
+{
+    cin >> noskipws;
+    char x;
+    for(int i = 0; i < 256; i++){
+        cin.read((char *)&frequency[i],4);
+    }
+
+    node * root = constructheap();
+    string code;
+    root->cb(codebook,code);
+
+    while(cin>>x)
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            if((x >> i) & 0x01)
+                code += '1';
+            else
+                code += '0';
+            for(int i = 0; i < 256; i++)
+            {
+                if(codebook[i] == code)
+                {
+                    if(frequency[i])
+                    {
+                              cout << (unsigned char) i;
+                              code.clear();
+                              frequency[i]--;
+                              break;
+                    }
+                    else
+                        return;
+                }
+            }
+        }
+    }
+
+
+}
+
+node * constructheap()
+{
+    heap minheap;
+    node *temp;
+    for(int i = 0; i < 256; i++)
+    {
+        if (frequency[i])
+        {
+            temp = new node(i, frequency[i]);
+            minheap.push(temp);
+        }
+    }
+
+    node * x;
+    node * y;
+    node * z;
+    while(minheap.size() > 1)
+    {
+        x = minheap.top();
+        minheap.pop();
+        y = minheap.top();
+        minheap.pop();
+        z = new node(x, y);
+        minheap.push(z);
+    }
+
+    return minheap.top();
+}
